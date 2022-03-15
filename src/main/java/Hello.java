aa96bb4729400d9f128beb33be8280f55863a07c
Rusu Dinu <dinu@codingshadows.com>
2022-03-15 19:06:29 +0200

    readme updates
    Signed-off-by: Rusu Dinu <dinu@codingshadows.com>

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        /*
+         - iau bol
+         - iau lapte din frigider
+         - pun lapte incalzit
+         - amestec
+         */
     }
 }
