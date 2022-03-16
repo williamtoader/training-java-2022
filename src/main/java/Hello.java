6732a041e83b2d27a137512787820b788ecfd871
Rusu Dinu <dinu@codingshadows.com>
2022-03-16 09:44:11 +0200

    session-1
    Signed-off-by: Rusu Dinu <dinu@codingshadows.com>

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        /*
-         - iau bol
-         - iau lapte din frigider
-         - pun lapte incalzit
-         - amestec
-         */
+        System.out.println("Hello world");
     }
 }
