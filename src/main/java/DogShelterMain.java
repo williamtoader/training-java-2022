2717185d721cc1d9ae5e19d19af63aecd5498976
Rusu Dinu <dinu@codingshadows.com>
2022-03-23 10:17:42 +0200

    finished dog shelter
    Signed-off-by: Rusu Dinu <dinu@codingshadows.com>

--- src/main/java/DogShelterMain.java
+++ src/main/java/DogShelterMain.java
@@ -1,5 +1,27 @@
 import java.util.*;
 
 public class DogShelterMain {
-    // 1p
+    public static void main(String[] args) {
+        LinkedList<AnimalX> animalList = new LinkedList<>();
+        TreeSet<AnimalX> animalSet = new TreeSet<>();
+
+        /*
+
+        [[value] next] -> [[otherValue] next] -> [[otherOtherValue] next] ...
+
+         */
+
+
+        AnimalX animal1 = new AnimalX();
+        AnimalX animal2 = new AnimalX();
+        AnimalX animal3 = new AnimalX();
+
+        animalList.add(animal1);
+        animalList.add(animal2);
+        animalList.add(animal3);
+
+        animalSet.add(animal1);
+        animalSet.add(animal2);
+        animalSet.add(animal3);
+    }
 }
